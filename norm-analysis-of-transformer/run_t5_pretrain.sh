56c50dcfc6fec93bb4feef93196eb4078cb232ed
Kevin Zhao <Kevin_Zhao@student.waylandps.org>
2022-01-31 15:13:54 -0500

    ishkur huggingface cache

--- norm-analysis-of-transformer/run_t5_pretrain.sh
+++ norm-analysis-of-transformer/run_t5_pretrain.sh
@@ -2,20 +2,20 @@ python t5_pretrain.py \
     --model_type t5 \
     --config_name t5-tiny-6L-4H \
     --tokenizer_name t5-small \
-    --cache_dir ~/.cache/huggingface/ \
+    --cache_dir /mnt/home/kzhao/.cache/huggingface/ \
     --dataset_name c4 \
     --dataset_config_name realnewslike \
     --max_seq_length 512 \
-    --preprocessing_num_workers 12 \
+    --preprocessing_num_workers 4 \
     --output_dir t5_pretraining_1 \
     --do_train \
     --do_eval \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --learning_rate 5e-5 \
+    --per_device_train_batch_size 128 \
+    --per_device_eval_batch_size 128 \
+    --learning_rate 1e-4 \
     --weight_decay 0.0 \
     --adafactor \
     --num_train_epochs 1 \
-    --warmup_steps 827990 \
+    --warmup_steps 20000 \
     --save_steps 500 \
     --eval_steps 500
