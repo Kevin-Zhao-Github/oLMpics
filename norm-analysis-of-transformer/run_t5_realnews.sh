5e90e24ccbb53adfca2f1a9c82c9b02ca4567c05
Kevin Zhao <zhaokevinusa@gmail.com>
2022-02-07 00:29:27 +0000

    Working realnewslike

--- norm-analysis-of-transformer/run_t5_realnews.sh
+++ norm-analysis-of-transformer/run_t5_realnews.sh
@@ -1,22 +1,23 @@
-rm -ri t5_pretraining_1
 accelerate launch t5_pretrain.py \
     --model_type t5 \
     --config_name t5-small-6L-8H \
     --tokenizer_name t5-small \
     --cache_dir /mnt/home/.cache/datasets/ \
-    --dataset_pickle_path processed_en10.pkl \
+    --dataset_pickle_path processed_realnewslike.pkl \
     --max_seq_length 128 \
     --preprocessing_num_workers 16 \
     --output_dir t5_pretraining_1 \
+    --overwrite_output_dir \
     --do_train \
     --do_eval \
-    --per_device_train_batch_size 64 \
-    --per_device_eval_batch_size 64 \
-    --learning_rate 0.003 \
+    --per_device_train_batch_size 48 \
+    --per_device_eval_batch_size 48 \
+    --gradient_accumulation_steps 2 \
+    --learning_rate 0.001 \
     --weight_decay 0.001 \
     --adafactor \
     --num_train_epochs 1 \
-    --warmup_steps 30000 \
-    --save_steps 15000 \
-    --eval_steps 8000 \
-    --logging_steps 100
+    --warmup_steps 3000 \
+    --save_steps 2000 \
+    --eval_steps 800 \
+    --logging_steps 20
